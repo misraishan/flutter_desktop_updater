76d2ee2534fbe28beb55f92ce662dbc6fa3e5c1c
Burak Karahan <burak.karahan@mail.ru>
2025-01-06 07:21:57 +0300

    add example

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import desktop_updater
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DesktopUpdaterPlugin.register(with: registry.registrar(forPlugin: "DesktopUpdaterPlugin"))
+}
