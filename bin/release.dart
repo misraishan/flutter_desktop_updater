19e02b9ba2df2996a566201c4207e7ab18cb52aa
Burak Karahan <burak.karahan@mail.ru>
2025-01-07 04:43:34 +0300

    feat: update dependencies, improve build script, and enhance zip file creation

--- bin/release.dart
+++ bin/release.dart
@@ -1,6 +1,7 @@
-import 'dart:io';
+import "dart:convert";
+import "dart:io";
 
-import 'package:archive/archive_io.dart';
+import "package:archive/archive_io.dart";
 
 Future<void> main(List<String> args) async {
   if (args.isEmpty) {
@@ -10,50 +11,88 @@ Future<void> main(List<String> args) async {
 
   final platform = args[0];
 
-  if (platform != 'macos' && platform != 'windows' && platform != 'linux') {
+  if (platform != "macos" && platform != "windows" && platform != "linux") {
     print("PLATFORM must be specified: macos, windows, linux");
     exit(1);
   }
 
   // Get current build name and number from pubspec.yaml
-  final pubspec = File('pubspec.yaml');
+  final pubspec = File("pubspec.yaml");
   final pubspecContent = await pubspec.readAsString();
-  
+
   // 1.0.0+1
-  final buildRegExp = RegExp(r'version: (.+)').firstMatch(pubspecContent)!.group(1);
-  final buildName = buildRegExp?.split('+').first;
-  final buildNumber = buildRegExp?.split('+').last;
+  final buildRegExp =
+      RegExp(r"version: (.+)").firstMatch(pubspecContent)!.group(1);
 
-  final buildCommand = [
-    'flutter',
-    'build',
-    platform,
-    '--dart-define',
-    'FLUTTER_BUILD_NAME=$buildName',
-    '--dart-define',
-    'FLUTTER_BUILD_NUMBER=$buildNumber'
-  ];
-
-  final result = await Process.run(buildCommand[0], buildCommand.sublist(1));
-  if (result.exitCode != 0) {
-    print("Build failed with exit code ${result.exitCode}");
+  if (buildRegExp == null) {
+    print("version not found in pubspec.yaml");
     exit(1);
   }
 
+  print("Building version ${buildRegExp.replaceAll('"', '')}");
+
+  final buildName = buildRegExp.replaceAll('"', "").split("+").first.trim();
+  final buildNumber = buildRegExp.replaceAll('"', "").split("+").last.trim();
+
+  // Get flutter path
+  final flutterPath = Platform.environment["FLUTTER_ROOT"];
+
+  // print current working directory
+  print("Current working directory: ${Directory.current.path}");
+
+  // Determine the Flutter executable based on the platform
+  var flutterExecutable = "flutter";
+  if (Platform.isWindows) {
+    flutterExecutable += ".bat";
+  }
+
+  final buildCommand = [
+    "$flutterPath${Platform.pathSeparator}bin${Platform.pathSeparator}$flutterExecutable",
+    "build",
+    platform,
+    "--dart-define",
+    "FLUTTER_BUILD_NAME=$buildName",
+    "--dart-define",
+    "FLUTTER_BUILD_NUMBER=$buildNumber",
+  ];
+
+  // Replace Process.run with Process.start to handle real-time output
+  final process =
+      await Process.start(buildCommand.first, buildCommand.sublist(1));
+
+  process.stdout.transform(utf8.decoder).listen(print);
+
+  process.stderr.transform(utf8.decoder).listen((data) {
+    stderr.writeln(data);
+  });
+
+  final exitCode = await process.exitCode;
+  if (exitCode != 0) {
+    stderr.writeln("Build failed with exit code $exitCode");
+    exit(1);
+  }
+
+  print("Build completed");
+
   // Found executable file name in build folder
-  final buildDir = Directory('build/$platform/x64/runner/Release');
-  final files = await buildDir.list().toList();
-  final file = files.firstWhere((file) => file.path.endsWith('.exe'));
+  final buildDir = Directory(
+    "build${Platform.pathSeparator}$platform${Platform.pathSeparator}x64${Platform.pathSeparator}runner${Platform.pathSeparator}Release",
+  );
+  final files = await buildDir.list(recursive: true).toList();
+  final file = files.firstWhere((file) => file.path.endsWith(".exe"));
 
-  final appName = file.path.split('/').last.split('.').first;
+  // Get only last part of the path
+  final appName = file.path.split(Platform.pathSeparator).last.split(".").first;
 
-  final buildPath = 'build/$platform/x64/runner/Release';
-  final zipPath = 'dist/$buildNumber/$appName-$platform-x64.zip';
+  final zipPath =
+      "dist${Platform.pathSeparator}$buildNumber${Platform.pathSeparator}$appName-$buildName+$buildNumber-$platform-x64.zip";
 
-  final encoder = ZipFileEncoder();
-  encoder.create(zipPath);
-  encoder.addDirectory(Directory(buildPath), includeDirName: false);
-  encoder.close();
+  // Create zip file with zipPath
+  final encoder = ZipFileEncoder()..create(zipPath);
+
+  await encoder.addDirectory(buildDir, includeDirName: false);
+
+  await encoder.close();
 
   print("Zip created to $zipPath");
 }
