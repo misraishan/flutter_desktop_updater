b265216aee082ce0453df766f92954a6ead4860c
Burak Karahan <burak.karahan@mail.ru>
2025-01-17 02:41:46 +0300

    feat: update license to MIT, improve app description, and update version number

--- example/lib/app.dart
+++ example/lib/app.dart
@@ -1,6 +1,5 @@
 import "package:desktop_updater/desktop_updater.dart";
 import "package:desktop_updater/updater_controller.dart";
-import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
 import "package:flutter/services.dart";
 
@@ -14,12 +13,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String _platformVersion = "Unknown";
   final _desktopUpdaterPlugin = DesktopUpdater();
-  String length = "";
-  String changedFiles = "";
-  String hashes = "";
-  ItemModel? appArchiveItem;
-  UpdateProgress? updateProgress;
-
   late DesktopUpdaterController _desktopUpdaterController;
 
   @override
@@ -29,7 +22,7 @@ class _HomePageState extends State<HomePage> {
 
     _desktopUpdaterController = DesktopUpdaterController(
       appArchiveUrl: Uri.parse(
-        "https://s3.eu-central-1.amazonaws.com/www.monolib.net/archive/desktop_updater/app-archive.json",
+        "https://www.yoursite.com/app-archive.json",
       ),
     );
   }
@@ -70,77 +63,14 @@ class _HomePageState extends State<HomePage> {
             child: Column(
               children: [
                 const Text(
-                  "Running on: 0.1.5+6",
+                  "Running on: 1.0.0+1",
                 ),
                 Text("Running on: $_platformVersion\n"),
-                ElevatedButton(
-                  onPressed: _desktopUpdaterPlugin.restartApp,
-                  child: const Text("Restart App"),
-                ),
-                OutlinedButton(
-                  onPressed: () {
-                    _desktopUpdaterPlugin
-                        .versionCheck(
-                      appArchiveUrl:
-                          "https://s3.eu-central-1.amazonaws.com/www.monolib.net/archive/desktop_updater/app-archive.json",
-                    )
-                        .then(
-                      (value) {
-                        print("App archive downloaded");
-                        print(value?.url);
-
-                        setState(() {
-                          appArchiveItem = value;
-                        });
-                      },
-                    );
-                  },
-                  child: const Text("Check version"),
-                ),
-                SelectableText("App archive item:\n${appArchiveItem?.url}\n"),
-                FilledButton(
-                  onPressed: () {
-                    _desktopUpdaterPlugin.updateApp(
-                      remoteUpdateFolder: appArchiveItem?.url ?? "",
-                      changedFiles: [],
-                    ).then(
-                      (value) {
-                        value.listen(
-                          (event) {
-                            setState(() {
-                              updateProgress = event;
-                            });
-                          },
-                          onDone: () {
-                            print("Update done");
-                          },
-                        );
-                      },
-                    );
-                  },
-                  child: const Text("Update App"),
-                ),
-                SelectableText(
-                  "Update progress:\n${updateProgress?.currentFile ?? ""}\n${updateProgress?.receivedBytes ?? 0}/${updateProgress?.totalBytes ?? 0}\n${updateProgress?.completedFiles ?? 0}/${updateProgress?.totalFiles ?? 0}",
-                ),
               ],
             ),
           ),
         ),
       ),
     );
-  }
-
-  @override
-  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
-    super.debugFillProperties(properties);
-    properties
-      ..add(StringProperty("length", length))
-      ..add(StringProperty("changedFiles", changedFiles))
-      ..add(StringProperty("hashes", hashes))
-      ..add(DiagnosticsProperty<ItemModel?>("appArchiveItem", appArchiveItem))
-      ..add(
-        DiagnosticsProperty<UpdateProgress?>("updateProgress", updateProgress),
-      );
   }
 }
